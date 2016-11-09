901f4ddd5f3ba3340b904f33e816e6bf4dfc44b4
Tony Thomas <01tonythomas@gmail.com>
2016-11-09 19:07:41 +0100

    Fix things, remove unwanted files - make the CSV

--- crawler.py
+++ crawler.py
@@ -1,10 +1,21 @@
+#!/usr/bin/python
+# Please makes sure you change the API Token
+
 import sys  
 from PyQt4.QtGui import *  
 from PyQt4.QtCore import *  
 from PyQt4.QtWebKit import *  
 from lxml import html 
 import requests
+import csv
 
+import requests
+s = requests.Session()
+
+api_token = "api-your-here"
+conduit = "https://phabricator.wikimedia.org/api/maniphest.query" 
+
+# since most of phab is rendered after page load, we need this cute thing
 class Render(QWebPage):  
   def __init__(self, url):  
     self.app = QApplication(sys.argv)  
@@ -22,8 +33,28 @@ r = Render(url)
 result = r.frame.toHtml()
 tree = html.fromstring(str(result.toAscii()))
 pbab_links_in_proj = tree.xpath('//ul[@data-meta="0_103"]/li//div[@class="phui-object-item-name"]/span/a/@href')
-for link in pbab_links_in_proj:
-	phab_link = "http://phabricator.wikimedia.org" + link
-	#now we have the link, we need to open this thing up and get the description and details, and post to the GCI website
+completeIds = []
 
-  
+# We need to remove that extra stuff from start of IDs 
+for link in pbab_links_in_proj:
+	completeIds.append(link[2:])
+	
+
+# We are doing a mass query to the phabricator API, making sure we do not ping again and again 
+args = { 'ids[]': completeIds, "api.token": api_token }
+req = requests.Request('POST', conduit, data=args)
+prepped = s.prepare_request(req)
+resp = s.send(prepped)
+resp.raise_for_status()
+completeResponse =  resp.json()['result'].values()
+
+# We are writing things to the CSV file now 
+with open('importedgci.csv', 'w') as csvfile:
+	fieldnames = ['name', 'description', 'external_url' ]
+	spamwriter = csv.DictWriter(csvfile, fieldnames=fieldnames)
+	for task in completeResponse:
+		row = {}
+		row['name'] = task['title'].encode('ascii', 'ignore').decode('ascii')
+		row['description'] = task['description'].encode('ascii', 'ignore').decode('ascii')
+		row['external_url'] = task['uri'].encode('ascii', 'ignore').decode('ascii')
+		spamwriter.writerow(row)
